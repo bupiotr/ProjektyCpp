cc1ecfb8652bf23248fa2075dc9e59dc7e3bddff
Bartekkur1 <Bartekkur1@o2.pl>
2017-02-21 19:51:50 +0100

    pobugowane ostro

---
+++ GeneratorHaseu/GeneratorHaseu/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GeneratorHaseu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wygeneruj Hasło";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 17);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "cyfry";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 40);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(47, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "litery";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(93, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(263, 42);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 110);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ilość znaków :";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(11, 87);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(99, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "znaki specjalne";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Made by Bika";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(11, 63);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(73, 17);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "duze litery";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 156);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "GeneratorHaseu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ GeneratorHaseu/GeneratorHaseu/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneratorHaseu
+{
+    public partial class Form1 : Form
+    {
+        string[] litery = new string[25] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        string[] literyDuze = new string[25] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+        string[] cyfry = new string[10] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+        char[] znakiSpecjalne = new char[15] { '!', '@', '#', '$', '%', '^', '&', '*', '(', '&', '/', '<', '>', '{', '}' };
+
+        int iloscZnakow;
+        Random generatorLosowych = new Random();
+        int cyfraLosowa;
+        int literaLosowa;
+        int dLiteraLosowa;
+        int znakLosowa;
+        int Losowa;
+        int iloscZnakowWTx;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //przycisk generujacy
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool czyToLiczba = Int32.TryParse(textBox2.Text, out iloscZnakow);
+            if (czyToLiczba)
+            {
+                textBox1.Text = "";
+                iloscZnakowWTx = 0;
+                while (iloscZnakowWTx != iloscZnakow)
+                { 
+                        iloscZnakowWTx = textBox1.Text.Length;
+
+                        cyfraLosowa = generatorLosowych.Next(0, cyfry.Length);
+                        literaLosowa = generatorLosowych.Next(0, litery.Length);
+                        dLiteraLosowa = generatorLosowych.Next(0, literyDuze.Length);
+                        znakLosowa = generatorLosowych.Next(0, znakiSpecjalne.Length);
+
+
+                        Losowa = generatorLosowych.Next(0, 3);
+
+                        if (Losowa == 0 && checkBox1.Checked) //cyfry
+                        {
+                            textBox1.Text += cyfry[cyfraLosowa];
+                        }
+                        if (Losowa == 1 && checkBox2.Checked) //litery
+                        {
+                            textBox1.Text += litery[literaLosowa];
+                        }
+                        if (Losowa == 2 && checkBox4.Checked) //duze litery
+                        {
+                            textBox1.Text += literyDuze[dLiteraLosowa];
+                        }
+                        if (Losowa == 3 && checkBox3.Checked) // znaki
+                        {
+                            textBox1.Text += znakiSpecjalne[znakLosowa];
+                        }
+                }
+            }
+            else
+            {
+                MessageBox.Show(textBox2.Text + " to nie jest liczba");
+            }
+        }
+        //cyfry
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        //litery
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        //znaki specjalne
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+        
+        }
+        //ilosc znakow
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //wygenerowane hasuo
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        //duzelitery
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
